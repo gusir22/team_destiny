ecfdc8dcbe957036d0db163218d0138feec729ca
Jexar <jexar.vision@gmail.com>
2024-10-18 21:43:55 -0400

    Creates pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,17 @@
 # pages/urls.py
 from django.urls import path
 
-from .views import HomePageView
+from .views import (
+    HomePageView,
+    SignupView,
+    LoginView,
+    ProfileView,
+)
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('/signup/', SignupView.as_view(), name='signup'),
+    path('/login/', LoginView.as_view(), name='login'),
+    path('/profile/', ProfileView.as_view(), name='profile'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,13 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class SignupView(TemplateView):
+    template_name = 'signup.html'
+    
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+class ProfileView(TemplateView):
+    template_name = 'profile.html'
