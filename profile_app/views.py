81bc1d2b46d60c3130faf528bf92e4d8c37b2ed3
Gabino Cortes <cortesgabinoxv@icloud.com>
2024-10-18 20:09:26 -0700

    created profile list page

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,11 +1,17 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from .models import (
     ProfilePage,
 )
 
 # Create your views here.
-class ProfilePageView(DetailView):
+class ProfilePageListView(ListView):
+    model = ProfilePage
+    template_name = "profiles_list.html"
+    context_object_name = "profiles"
+
+
+class ProfilePageDetailView(DetailView):
     model = ProfilePage
     template_name = "profile_page.html"
     context_object_name = "profile"
